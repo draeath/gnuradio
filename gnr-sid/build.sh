b2e7157b425cfbbb6d2d66832bd2d70411b44166
Paul Bransford <draeath@gmail.com>
2017-11-25 23:32:46 -0500

    include uid/gid with name autodetect

--- gnr-sid/build.sh
+++ gnr-sid/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker build -t gnr-sid --build-arg dkuser=$(id -un) --build-arg dkgroup=$( id -gn) .
+docker build -t gnr-sid \
+  --build-arg dkuser=$(id -un) \
+  --build-arg dkuid=$(id -u) \
+  --build-arg dkgroup=$(id -gn) \
+  --build-arg dkgid=$(id -g) \
+  .
